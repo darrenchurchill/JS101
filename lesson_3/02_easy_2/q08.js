806e43e56faf906616b7eb2ac291621bbed74050
Darren Churchill <darrenmchurchill@gmail.com>
2023-02-20 10:09:32 -0800

    Add Lesson 3: Easy 2 - Question 8

---
+++ lesson_3/02_easy_2/q08.js
@@ -0,0 +1,17 @@
+/**
+ * Darren Churchill
+ *
+ * JS101 Lesson 3
+ * Practice Problems: Easy 2
+ *
+ * Question 8
+ *
+ * How would you check whether the two objects below are arrays?
+ *
+ */
+
+let numbers = [1, 2, 3, 4];
+let table = { field1: 1, field2: 2, field3: 3, field4: 4 };
+
+console.log(Array.isArray(numbers));
+console.log(Array.isArray(table));
