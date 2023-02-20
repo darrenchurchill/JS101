a1643ac50fed97a87494b24603cd8cd3be8b3862
Darren Churchill <darrenmchurchill@gmail.com>
2023-02-19 18:28:03 -0800

    Add Lesson 3: Easy 2 - Question 5

---
+++ lesson_3/02_easy_2/q05.js
@@ -0,0 +1,16 @@
+/**
+ * Darren Churchill
+ *
+ * JS101 Lesson 3
+ * Practice Problems: Easy 2
+ *
+ * Question 5
+ *
+ * Given an array of numbers, mutate the array by removing the number at
+ * index 2.
+ *
+ */
+
+let array = [1, 2, 3, 4, 5];
+array.splice(2, 1);
+console.log(array);
