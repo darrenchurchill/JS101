92e429f249689a0d2f4bca33af07e7d2fde2657c
Darren Churchill <darrenmchurchill@gmail.com>
2023-02-19 17:40:25 -0800

    Add Lesson 3: Easy 1 - Question 7

---
+++ lesson_3/01_easy_1/q07.js
@@ -0,0 +1,17 @@
+/**
+ * Darren Churchill
+ *
+ * JS101 Lesson 3
+ * Practice Problems: Easy 1
+ *
+ * Question 7
+ *
+ * Determine whether the name `Dino` appears in the strings below.
+ *
+ */
+
+let str1 = "Few things in life are as important as house training your pet dinosaur.";
+let str2 = "Fred and Wilma have a pet dinosaur named Dino.";
+
+console.log(str1.includes('Dino'));
+console.log(str2.includes('Dino'));
