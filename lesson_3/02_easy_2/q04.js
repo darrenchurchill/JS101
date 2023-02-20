eb6f7a6d89a6e820e15e3ec9249b37fd6e03453b
Darren Churchill <darrenmchurchill@gmail.com>
2023-02-19 18:25:33 -0800

    Add Lesson 3: Easy 2 - Question 4

---
+++ lesson_3/02_easy_2/q04.js
@@ -0,0 +1,19 @@
+/**
+ * Darren Churchill
+ *
+ * JS101 Lesson 3
+ * Practice Problems: Easy 2
+ *
+ * Question 4
+ *
+ * Starting with the given string, show 2 different ways to put "Four score and"
+ * in front of it.
+ *
+ */
+
+let famousWords = "seven years ago...";
+
+let prefix = "Four score and ";
+
+console.log(prefix + famousWords);
+console.log(`${prefix}${famousWords}`);
