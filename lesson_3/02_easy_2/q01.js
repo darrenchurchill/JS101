7f9fa3ae5f93ee11dd506eee1604f0396ce3f651
Darren Churchill <darrenmchurchill@gmail.com>
2023-02-19 18:02:13 -0800

    Add Lesson 3: Easy 2 - Question 1

---
+++ lesson_3/02_easy_2/q01.js
@@ -0,0 +1,16 @@
+/**
+ * Darren Churchill
+ *
+ * JS101 Lesson 3
+ * Practice Problems: Easy 2
+ *
+ * Question 1
+ *
+ * Given a string, return a new string with every occurrence of the word
+ * "important" with "urgent".
+ *
+ */
+
+let advice = "Few things in life are as important as house training your pet dinosaur.";
+
+console.log(advice.replace('important', 'urgent'));
