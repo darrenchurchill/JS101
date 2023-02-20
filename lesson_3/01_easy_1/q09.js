adf5fd350518d0bcfaa7cd911fa2cceedab6cd52
Darren Churchill <darrenmchurchill@gmail.com>
2023-02-19 17:44:49 -0800

    Add Lesson 3: Easy 1 - Question 9

---
+++ lesson_3/01_easy_1/q09.js
@@ -0,0 +1,16 @@
+/**
+ * Darren Churchill
+ *
+ * JS101 Lesson 3
+ * Practice Problems: Easy 1
+ *
+ * Question 9
+ *
+ * How can we add multiple items (`'Dino'` and `'Hoppy'`) to the following
+ * array?
+ *
+ */
+
+let flintstones = ["Fred", "Barnet", "Wilma", "Betty", "Bambam", "Pebbles"];
+flintstones.push("Dino", "Hoppy");
+console.log(flintstones);
